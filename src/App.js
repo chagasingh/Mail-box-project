66fa4d1e7fd45b77a7e8f3316c24a9f73b2b2638
chagansingh <chagansingh9876@gmail.com>
2023-02-21 12:33:09 +0530

    login form added

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import AuthForm from './components/AuthForm/AuthForm'
+import Layout from './components/Layout/Layout'
+import { Fragment } from 'react';
+
+function App() {
+
+  return (
+    <Fragment>
+      <Layout/>
+      <AuthForm/>
+    </Fragment>
+  );
+}
+
+export default App;
+
